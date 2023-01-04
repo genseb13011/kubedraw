acc8ae98ff7394140e91d43cc0bedb81d8c06367
unknown <genesta.sebastien@gmail.com>
2023-01-04 22:59:33 +0100

    try generate first diagram

---
+++ draw_diagram.py
@@ -0,0 +1,11 @@
+from diagrams import Cluster, Diagram
+from diagrams.k8s.clusterconfig import HPA
+from diagrams.k8s.compute import Deployment, Pod, ReplicaSet
+from diagrams.k8s.network import Ingress, Service
+
+with Diagram("K8S diagram", show=False):
+  with Cluster("Namespace1"):
+    net = Ingress("ingress1") >> Service("Service1")
+    with Cluster("Deployment1"):
+      pods = [Pod("pod1"),Pod("pod2")]
+  net >> pods
