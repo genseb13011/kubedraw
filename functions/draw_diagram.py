dd847b879113364a893ab58815c39bde72c781f6
unknown <genesta.sebastien@gmail.com>
2023-01-04 23:51:35 +0100

    draw diagram test

--- functions/draw_diagram.py
+++ functions/draw_diagram.py
@@ -22,11 +22,11 @@ def draw_diagram():
   namespaces = c.fetchall()
 
   with Diagram("K8S diagram", show=False):
-  for namespace in namespaces:
-    with Cluster("%s" % (namespace)):
-      net = Ingress("ingress1") >> Service("Service1")
-      with Cluster("Deployment1"):
-        pods = [Pod("pod1"),Pod("pod2")]
-    net >> pods
+    for namespace in namespaces:
+      with Cluster("%s" % (namespace)):
+        net = Ingress("ingress1") >> Service("Service1")
+        with Cluster("Deployment1"):
+          pods = [Pod("pod1"),Pod("pod2")]
+      net >> pods
 
   conn.commit()
