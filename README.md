6a71e722a2a80b3e5b0bae15d9c9fb9e406c2028
unknown <genesta.sebastien@gmail.com>
2023-01-04 23:50:44 +0100

    draw diagram test

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Requirements:
 1) Need to install graphviz on the server
 
 apt-get install graphviz
+
+2) generate read serviceaccount
--- functions/draw_diagram.py
+++ functions/draw_diagram.py
@@ -2,12 +2,31 @@ from diagrams import Cluster, Diagram
 from diagrams.k8s.clusterconfig import HPA
 from diagrams.k8s.compute import Deployment, Pod, ReplicaSet
 from diagrams.k8s.network import Ingress, Service
+import sqlite3
 
 def draw_diagram():
 
+  conn = None
+  try:
+    conn = sqlite3.connect(db_name)
+  except Error as e:
+    print(e)
+
+  c = conn.cursor()
+
+  c.execute("""
+          SELECT * FROM namespaces
+          """
+          )
+
+  namespaces = c.fetchall()
+
   with Diagram("K8S diagram", show=False):
-    with Cluster("Namespace1"):
+  for namespace in namespaces:
+    with Cluster("%s" % (namespace)):
       net = Ingress("ingress1") >> Service("Service1")
       with Cluster("Deployment1"):
         pods = [Pod("pod1"),Pod("pod2")]
     net >> pods
+
+  conn.commit()
