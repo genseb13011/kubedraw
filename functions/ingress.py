d5808ea092f0ebe85ba84277f977289c2d496416
unknown <genesta.sebastien@gmail.com>
2023-01-17 01:26:23 +0100

    add service to ingress

--- functions/ingress.py
+++ functions/ingress.py
@@ -41,7 +41,7 @@ def insert_ingress(db_name):
     services_result=""
     i=0
     while i < services_length:
-      services_result.append(services[i]",")
+      services_result.append(services[i]+",")
 
     c.execute("""
             INSERT INTO ingress(name,namespace,ingressclass,services)
